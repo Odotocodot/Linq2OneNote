e5cb1471ad8dbc61d8f6a89e3f78952f26ee2207
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2024-05-27 16:07:59 +0100

    Refactor parser tests

---
+++ Odotocodot.OneNote.Linq.Tests/ParserTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Odotocodot.OneNote.Linq.Tests
+{
+	[TestFixture]
+	[TestOf(typeof(OneNoteParser))]
+	public class ParserTests
+	{
+		[Test]
+		[TestCase(typeof(OneNoteNotebook), 4)]
+		[TestCase(typeof(OneNoteSectionGroup), 7)]
+		[TestCase(typeof(OneNoteSection), 20)]							
+		[TestCase(typeof(OneNotePage), 28)]
+		public void ParseNotebooks_CorrectNumberOfItems(Type itemType, int expectedCount)
+		{
+			var xml = File.ReadAllText(@"Inputs\Notebooks.xml");
+			
+			var result = OneNoteParser.ParseNotebooks(xml);
+			var items = result.Traverse(item => item.GetType() == itemType);
+			
+			Assert.AreEqual(expectedCount, items.Count());
+		}
+	}
+}
